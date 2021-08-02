d179cd5130bf4be1aba4b739164695aff481cc7d
brianleect <63389110+brianleect@users.noreply.github.com>
2021-08-02 11:32:19 +0800

    [Feat] Tele integration + Modifiable view_num

---
+++ params.py
@@ -0,0 +1,12 @@
+SEND_TELE = False # Set to true if sending Tele, if false it will just print to console
+
+# Max Symbols Viewed
+VIEW_NUM = 25
+
+# Used for telegram bot updates
+token = ''  # Insert token obtained from @BotFather here
+chat_id = 0 # Insert Chat ID
+
+
+# Debug Params (Avoid Modifying if no error)
+SEND_TELEGRAM_FAIL_INTERVAL = 2
