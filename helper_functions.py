9dfe3fde561dd84dbcfb8c214732fb33d1829dde
brianleect <63389110+brianleect@users.noreply.github.com>
2021-08-02 15:47:05 +0800

    [Improvement] Add conversion for days

--- helper_functions.py
+++ helper_functions.py
@@ -3,5 +3,6 @@ def durationToSeconds(str_dur):
     if unit == 's': unit = 1
     elif unit == 'm': unit = 60
     elif unit == 'h': unit = 3600
+    elif unit == 'd': unit = 3600*24
 
     return  int(str_dur[:-1]) * unit
